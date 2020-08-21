c6e7de495b497a8db982addcb650acf4eb00b1e8
s3668087 <s3668087@student.rmit.edu.au>
2020-08-21 21:25:37 +1000

    Update

--- Q1/outdated/testenv.py
+++
@@ -1,8 +0,0 @@
-while True:
-    person1 = getpass.getpass(prompt="\nPerson 1 Code: ")
-    if len(person1) != 5:
-        print("You must enter 5 digits!")
-    if not person1.isdigit():
-        print("Your input must be a number!")
-    elif len(person1) == 5:
-        break;
